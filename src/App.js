a776c742c78125e4debeba2a60f03b4720063ef5
Sergey <sergr377@gmail.com>
2020-04-25 22:28:02 +0300

    Add withRouter function to ProfileContainer

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App(props) {
           <div className='app-wrapper-content'>
             <Route path='/dialogs'
               render={() => <DialogsContainer />} />
-            <Route path='/profile'
+            <Route path='/profile/:userId?'
               render={() => <ProfileContainer />} />
             <Route path='/users'
               render={() =><UsersContainer />} />
