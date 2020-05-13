4ac2703c92ac372ccb90337e5ac7b8b42716ac5a
Sergey <sergr377@gmail.com>
2020-05-13 12:17:31 +0300

    bug fix history redirected from login

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -12,9 +12,9 @@ class ProfileContainer extends React.Component {
         let userId = this.props.match.params.userId;
         if (!userId) {
             userId = this.props.authorizedUserId;
-            // if (!userId) {
-            //     this.props.history.push('/login')
-            // }
+            if (!userId) {
+                this.props.history.push('/login')
+            }
         }
         this.props.getUserProfile(userId);
         this.props.getStatus(userId);
@@ -33,7 +33,7 @@ class ProfileContainer extends React.Component {
 let mapStateToProps = (state) => ({
     profile: state.profilePage.profile,
     status: state.profilePage.status,
-    authorizedUserId: state.auth.isUserId,
+    authorizedUserId: state.auth.userId,
     isAuth: state.auth.isAuth
 })
 export default compose(
